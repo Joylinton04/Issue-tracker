1b29cdfb6d1cb75467b464b5f4ab2b205dd3cd31
Joylinton04 <adujoy05@gmail.com>
2025-10-13 14:37:06 +0000

    added markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,35 @@
-'use client'
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+"use client";
+import { Button, TextField } from "@radix-ui/themes";
+import React, { FormEvent, useState } from "react";
+import dynamic from "next/dynamic";
+import "easymde/dist/easymde.min.css";
+
+// Dynamically import SimpleMDE with ssr: false
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 const NewIssuePage = () => {
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    console.log("Submitting Issue:", { description });
+  };
+
   return (
-    <div className="max-w-xl space-y-4">
-        <TextField.Root placeholder="title" >
-        </TextField.Root>
-        <TextArea placeholder="Description"/>
-      <Button>Submit New Issue</Button>
-    </div>
+    <form onSubmit={handleSubmit} className="max-w-xl space-y-4">
+      <TextField.Root placeholder="title"></TextField.Root>
+
+      <SimpleMDE
+        value={description}
+        onChange={(e) => setDescription(e)}
+        placeholder="Description"
+      />
+
+      <Button type="submit">Submit New Issue</Button>
+    </form>
   );
 };
 
